d5d4021e0aad072277e9ce20ee4818edd17c85c4
LostSnake <66491205+LostSnake@users.noreply.github.com>
2022-07-04 21:11:50 +0300

    Square of Triangle and Circle

---
+++ Square.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestTaskForHh
+{
+
+    
+    public static class Square
+    {
+        static double PerimValue;
+        static double P = 3.14;
+
+        static public double PerimCircle(int Radius)
+        {
+            
+            int RadQur = Convert.ToInt32(Math.Pow(Radius, 2));
+            PerimValue = (P * RadQur);
+
+            return PerimValue;
+
+        }
+
+
+        static public double SquareOfTri(int a, int b, int c)
+        {
+            int p = (a + b + c) / 2;
+
+
+            double C = Math.Pow(c, 2); 
+
+            if (C == (a*a) + (b*b))
+            {
+                Console.WriteLine("Треугольник прямоугольный");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не прямоугольный");
+            }
+
+            PerimValue = Math.Sqrt(p * (p-a) * (p-b)*(p-c));
+
+            return PerimValue;
+        }
+
+        
+
+    }
+}
